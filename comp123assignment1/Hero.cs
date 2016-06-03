61e31229f7ca19a12d53e69c1cc0a992ec7f6450
jimmymcperson <undeadwalrus@hotmail.com>
2016-06-03 11:14:17 -0400

    Initial Commit

---
+++ comp123assignment1/Hero.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace comp123assignment1
+    {
+
+    /** <summary>
+     * This class defines a generic hero.
+     * </summary>
+     * 
+     * @class Hero
+     */
+    public class Hero
+        {
+
+        // CONSTRUCTORS =====================================================
+
+        /** <summary>
+         * This is the default/empty constructor
+         * </summary>
+         *
+         * @constructor Hero
+         */
+        public Hero()
+            {
+            Console.WriteLine("A hero has been generated!");
+            }
+
+        }
+    }
---
+++ comp123assignment1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * Author: Jonathan Hao
+ * Date: June 3, 2016
+ * Date Modified: June 3, 2016
+ * Description: This is a demonstration of the basics of object-oriented program.
+ * Version: 0.0.1 - Initial Commit
+ */
+namespace comp123assignment1
+    {
+
+    /** <summary>
+     * This is the "driver" class for our program
+     * </summary>
+     * 
+     * @class Program
+     */
+    public class Program
+        {
+
+        /** <summary>
+         * This is the "main" method
+         * </summary>
+         * 
+         * @method Main
+         * @param {string} args
+         * @returns {void}
+         */
+        public static void Main(string[] args)
+            {
+            Hero heroOfJustice = new Hero();
+            }
+        }
+    }

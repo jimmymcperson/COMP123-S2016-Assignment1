688affd723cc97cbee3e79a0c05d1900939a6f6a
jimmymcperson <undeadwalrus@hotmail.com>
2016-06-03 11:50:38 -0400

    Changed description and worked on Hero class

--- comp123assignment1/Hero.cs
+++ comp123assignment1/Hero.cs
@@ -17,6 +17,34 @@ namespace comp123assignment1
     public class Hero
         {
 
+        // PRIVATE INSTANCE VARIABLES =====================================================
+
+        private int _strength;
+        private int _speed;
+        private int _health;
+        private string _name;
+
+        // PUBLIC PROPTERTIES =====================================================
+
+        /** <summary>
+         * This is the property that read/writes the _name field
+         * </summary>
+         *
+         * @property {string} Name
+         */
+        public string Name
+            {
+            get
+                {
+                return this._name;
+                }
+            set
+                {
+                this._name = value;
+                Console.WriteLine("Name set as: '{0}'" , this._name);
+                }
+            }
+
         // CONSTRUCTORS =====================================================
 
         /** <summary>
@@ -27,7 +55,44 @@ namespace comp123assignment1
          */
         public Hero()
             {
+            _initializeAll();
             Console.WriteLine("A hero has been generated!");
+            }
+
+         // PRIVATE METHODS =====================================================
+
+        /** <summary>
+         * This method initializes all variables at a default value
+         * </summary>
+         *
+         * @method initializeAll
+         * @returns {void}
+         */
+        private void _initializeAll()
+            {
+//temp stats until I figure out how to randomize
+             _strength = 1;
+             _speed = 1;
+             _health = 100;
+             this.Name = "Hero";
+            }
+
+         // PUBLIC METHODS =====================================================
+
+        /** <summary>
+         * This method displays the hero's parameters
+         * </summary>
+         *
+         * @method Show
+         * @returns {void}
+         */
+        public void Show()
+            {
+// gotta make it pretty
+            Console.WriteLine(this.Name);
+            Console.WriteLine("STR " + _strength);
+            Console.WriteLine("SPD " + _speed);
+            Console.WriteLine("HP " + _health);
             }
 
         }
--- comp123assignment1/Program.cs
+++ comp123assignment1/Program.cs
@@ -8,8 +8,8 @@ using System.Threading.Tasks;
  * Author: Jonathan Hao
  * Date: June 3, 2016
  * Date Modified: June 3, 2016
- * Description: This is a demonstration of the basics of object-oriented program.
- * Version: 0.0.1 - Initial Commit
+ * Description: This is a demonstration of the basics of object-oriented programming.
+ * Version: 0.0.2 - Changed description and worked on Hero class
  */
 namespace comp123assignment1
     {
@@ -34,6 +34,16 @@ namespace comp123assignment1
         public static void Main(string[] args)
             {
             Hero heroOfJustice = new Hero();
+
+            Console.WriteLine();
+
+            heroOfJustice.Name = "Hero of Justice";
+
+            Console.WriteLine();
+
+            heroOfJustice.Show();
+
+            Console.WriteLine();
             }
         }
     }
